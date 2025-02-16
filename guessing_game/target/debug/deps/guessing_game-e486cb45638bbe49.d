d12173a0b5018b9431ccd6b05d729e9aa9ded47d
3xg3lin <krmutku76@gmail.com>
2025-02-16 22:15:01 +0300

    Update guessing_game/

--- guessing_game/target/debug/deps/guessing_game-e486cb45638bbe49.d
+++ guessing_game/target/debug/deps/guessing_game-e486cb45638bbe49.d
@@ -1,5 +1,5 @@
-/home/ghost/Documents/myRustProject/guessing_game/target/debug/deps/libguessing_game-e486cb45638bbe49.rmeta: src/main.rs
+/home/ghost/Documents/Github/myRustProject/guessing_game/target/debug/deps/libguessing_game-e486cb45638bbe49.rmeta: src/main.rs
 
-/home/ghost/Documents/myRustProject/guessing_game/target/debug/deps/guessing_game-e486cb45638bbe49.d: src/main.rs
+/home/ghost/Documents/Github/myRustProject/guessing_game/target/debug/deps/guessing_game-e486cb45638bbe49.d: src/main.rs
 
 src/main.rs:
