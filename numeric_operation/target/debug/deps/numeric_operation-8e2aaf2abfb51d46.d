1ab0f4e5285f7726512290ce24df83e066559629
3xg3lin <krmutku76@gmail.com>
2025-02-17 16:01:34 +0300

    Create numeric_operation/

---
+++ numeric_operation/target/debug/deps/numeric_operation-8e2aaf2abfb51d46.d
@@ -0,0 +1,5 @@
+/home/ghost/Documents/Github/myRustProject/numeric_operation/target/debug/deps/numeric_operation-8e2aaf2abfb51d46: src/main.rs
+
+/home/ghost/Documents/Github/myRustProject/numeric_operation/target/debug/deps/numeric_operation-8e2aaf2abfb51d46.d: src/main.rs
+
+src/main.rs:
---
+++ numeric_operation/target/debug/numeric_operation.d
@@ -0,0 +1,1 @@
+/home/ghost/Documents/Github/myRustProject/numeric_operation/target/debug/numeric_operation: /home/ghost/Documents/Github/myRustProject/numeric_operation/src/main.rs
