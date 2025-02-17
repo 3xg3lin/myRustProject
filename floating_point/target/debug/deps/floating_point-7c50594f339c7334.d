8f35bbda7075b39de21ccd4431f58c61e3281de7
3xg3lin <krmutku76@gmail.com>
2025-02-17 15:54:48 +0300

    Create floating_point/

---
+++ floating_point/target/debug/deps/floating_point-7c50594f339c7334.d
@@ -0,0 +1,5 @@
+/home/ghost/Documents/Github/myRustProject/floating_point/target/debug/deps/floating_point-7c50594f339c7334: src/main.rs
+
+/home/ghost/Documents/Github/myRustProject/floating_point/target/debug/deps/floating_point-7c50594f339c7334.d: src/main.rs
+
+src/main.rs:
---
+++ floating_point/target/debug/floating_point.d
@@ -0,0 +1,1 @@
+/home/ghost/Documents/Github/myRustProject/floating_point/target/debug/floating_point: /home/ghost/Documents/Github/myRustProject/floating_point/src/main.rs
