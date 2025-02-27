1f9b9825d5a0cbecc597e5179ce391d8a6879aa9
3xg3lin <krmutku76@gmail.com>
2025-02-27 22:20:55 +0300

    Create expression/

---
+++ expression/target/debug/deps/expression-9aea1d5c33b3367b.d
@@ -0,0 +1,5 @@
+/home/ghost/Documents/my_Github/myRustProject/expression/target/debug/deps/expression-9aea1d5c33b3367b: src/main.rs
+
+/home/ghost/Documents/my_Github/myRustProject/expression/target/debug/deps/expression-9aea1d5c33b3367b.d: src/main.rs
+
+src/main.rs:
---
+++ expression/target/debug/expression.d
@@ -0,0 +1,1 @@
+/home/ghost/Documents/my_Github/myRustProject/expression/target/debug/expression: /home/ghost/Documents/my_Github/myRustProject/expression/src/main.rs
